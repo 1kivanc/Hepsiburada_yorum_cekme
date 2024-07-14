6e9457d1590a462baff1f666f7f1022184b4d3a0
1kivanc <kivanc0556@gmail.com>
2024-07-15 00:25:18 +0300

    data cleaning

--- yorumCekmeBotu.py
+++ yorumCekmeBotu.py
@@ -1,9 +1,17 @@
 import time
+import re
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
+
+def clean_text(text):
+    text = re.sub(r'\s+', ' ', text) 
+    text = re.sub(r'[^\w\s]', '', text) 
+    text = text.strip() 
+    text = text.lower()  
+    return text
 
 def hepsiburada_yorum_cek(url):
     global_delay = 0.5
@@ -11,7 +19,7 @@ def hepsiburada_yorum_cek(url):
 
     try:
         driver.get(url)
-        time.sleep(5)  
+        time.sleep(5)
         print('Ürünün yorum sayfasına gidildi')
 
         current_page_number = 1  
@@ -19,15 +27,16 @@ def hepsiburada_yorum_cek(url):
             time.sleep(3)
             pagination_element = driver.find_element(By.XPATH, '/html/body/div[2]/main/div[2]/div/div/div/div/div/div/div[1]/div[2]/div[2]/div[6]/div[3]/div[2]/div/ul')
             page_elements = pagination_element.find_elements(By.TAG_NAME, 'li')
-            total_pages = int(page_elements[-1].find_element(By.TAG_NAME, 'span').text) 
+            total_pages = int(page_elements[-1].find_element(By.TAG_NAME, 'span').text)
 
             print(f'Şu anki sayfa: {current_page_number}, Toplam sayfa: {total_pages}')
 
             for i in range(1, 11):
                 try:
                     yorum = driver.find_element(By.XPATH, f'/html/body/div[2]/main/div[2]/div/div/div/div/div/div/div[1]/div[2]/div[2]/div[6]/div[3]/div[1]/div[{i}]/div[2]/div[2]/span').text
+                    cleaned_yorum = clean_text(yorum)
                     with open("yorumlar.txt", "a", encoding='utf-8') as yorum_file:
-                        yorum_file.write(yorum + '\n')
+                        yorum_file.write(cleaned_yorum + '\n')
                     time.sleep(global_delay)
                 except Exception as e:
                     print(f'Yorum çekilirken hata oluştu: {str(e)}')
@@ -37,7 +46,7 @@ def hepsiburada_yorum_cek(url):
                 next_page_url = url + f"?sayfa={current_page_number}"
                 driver.get(next_page_url)
                 print(f'Sonraki sayfa ({current_page_number}) yükleniyor...')
-                time.sleep(3) 
+                time.sleep(3)
                 pagination_element = driver.find_element(By.XPATH, '/html/body/div[2]/main/div[2]/div/div/div/div/div/div/div[1]/div[2]/div[2]/div[6]/div[3]/div[2]/div/ul')
                 page_elements = pagination_element.find_elements(By.TAG_NAME, 'li')
                 total_pages = int(page_elements[-1].find_element(By.TAG_NAME, 'span').text)
